dc948fa0f661c10388efac5375bbd98c58b3e829
foospidy <phillip@foospidy.com>
2018-09-14 15:27:59 -0400

    make sound play platform independent; add themes!

--- sigsci-sounds.go
+++ sigsci-sounds.go
@@ -6,7 +6,6 @@ import (
     "os"
     "io/ioutil"
     "encoding/json"
-    "os/exec"
     "runtime"
     "sync"
     "net/http"
@@ -14,13 +13,17 @@ import (
     "net/http/cookiejar"
     "strings"
     "time"
+
+    "github.com/faiface/beep"
+    "github.com/faiface/beep/wav"
+	"github.com/faiface/beep/speaker"
 )
 
 const (
     defaultConfigFile   = "./sigsci-sounds.conf"
     apiURL             = "https://dashboard.signalsciences.net/api/v0"
     loginEndpoint      = apiURL + "/auth/login"
-    interval            = 600
+    interval            = 300
 )
 
 // Config Configuration for sigsci-sounds
@@ -110,32 +113,13 @@ func initConfig() Config {
     return c
 }
 
-func main() {
-    fmt.Println("Initiating SigSci Sound!")
-    fmt.Println("Enjoy the soothing sounds of attacks and anomalies...")
-    fmt.Println("Press Ctrl+C to terminate.")
-    runtime.GOMAXPROCS(2)
-
-    var wg sync.WaitGroup
-    var config Config
-    var session []*http.Cookie
-
-    // initialize configuration
-    config = initConfig()
-
-    // add WaitGroups for the number of tags in the configuration
-    // concurrency implementaiton based on https://www.goinggo.net/2014/01/concurrency-goroutines-and-gomaxprocs.html
-    wg.Add(len(config.Tags))
-	
-    // set Timeseries endpoint
-    var timeseriesEndpoint = apiURL + "/corps/" + config.CorpName + "/sites/" + config.SiteName + "/timeseries/requests"
-
-    // get credentials from configuration and authenticate to SigSci API
+func MakeRequest(username string, password string, endpoint string, ch chan<-string) {
     form := url.Values{
-        "email":    []string{config.Username},
-        "password": []string{config.Password},
+        "email":    []string{username},
+        "password": []string{password},
     }
 
+    var session []*http.Cookie
     req, _ := http.NewRequest("POST", loginEndpoint, strings.NewReader(form.Encode()))
     req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
     client                          := &http.Client{}
@@ -152,109 +136,110 @@ func main() {
     }
 
     // get session cookie and store in cookie jar
-    session  = resp.Cookies()
-    jar, _  := cookiejar.New(nil)
-    u, _    := url.Parse(timeseriesEndpoint)
+    session = resp.Cookies()
+    jar, _ := cookiejar.New(nil)
+    u, _ := url.Parse(endpoint)
     jar.SetCookies(u, session)
     client.Jar = jar
-    
-    // for each tag in configuration launch a goroutine
-    for i := range(config.Tags) {
-        // get tag configuration
-        tag   := config.Tags[i].Name
-        sound := config.Tags[i].Sound
-        text  := config.Tags[i].Text
 
-        // go go gadget goroutine!
-        go func() {
-            // defer WaitGroup magic
-            defer wg.Done()
+    // call timeseries API endpoint to get json payload
+    req, _ = http.NewRequest("GET", endpoint, nil)
+    resp, clientErr := client.Do(req)
 
-            // initialize needed variables
-            var command, content string
-            var now        = int32(time.Now().Unix())
-            var fromUntil = fmt.Sprintf("&from=%d&until=%d", now - interval, now)
-
-            // start infinate loop
-            for {
-                // call timeseries API endpoint to get json payload
-                req, _          = http.NewRequest("GET", timeseriesEndpoint + "?&tag=" + tag + fromUntil, nil)
-                resp, clientErr := client.Do(req)
-
-                if clientErr != nil {
-                    log.Fatal(fmt.Sprintf("Error connecting to API: %v", clientErr))
-                }
-
-                defer resp.Body.Close()
-
-                payload, ioErr := ioutil.ReadAll(resp.Body)
-
-                if ioErr != nil {
-                    log.Fatal(fmt.Sprintf("Unable to read API response: %v", ioErr))
-                }
-
-                // initialize Timeseries object and load json payload data
-                var t Timeseries
-
-                unmarshalErr := json.Unmarshal(payload, &t)
-                if unmarshalErr != nil {
-                    log.Fatal(unmarshalErr)
-                }
-                
-                // determine which binary and content to use
-                if("say" == sound) {
-                    command = "say"
-                    content = text
-                } else {
-                    command = "afplay"
-                    content = sound
-
-                    // verify sound file exists
-                    _, fileErr := os.Stat(content)
-	
-                    if fileErr != nil {
-                        log.Fatal("Sound file is missing: ", content)
-                    }
-                }
-                
-                // look for binary to execute
-                binary, lookErr := exec.LookPath(command)
-
-                if lookErr != nil {
-                    log.Fatal(lookErr)
-                }
-
-                // set content as an argument for binary
-                args := []string{content}
-                
-                // for each timeseries value of data,
-                // sleep for 1 second and
-                // if value is greator than 0, then
-                // play sound
-                if(0 != len(t.Data)){
-                    for i := range(t.Data[0].Data) {
-                        time.Sleep(time.Second)
-                        if(t.Data[0].Data[i] > 0) {
-                            execErr := exec.Command(binary, args...).Run()
-
-                            if execErr != nil {
-                                fmt.Println(tag)
-                                log.Fatal(execErr)
-                            }
-                        }
-                    }
-
-                    // set new from and until values for next API call 
-                    fromUntil = fmt.Sprintf("&from=%d&until=%d", t.Data[0].Until, t.Data[0].Until + interval)
-                }
-
-                // sleep for interval before doing it all over again and playing more sounds for this tag
-                time.Sleep(time.Second * interval)
-            }
-        } ()
+    if clientErr != nil {
+        log.Fatal(fmt.Sprintf("Error connecting to API: %v", clientErr))
     }
+
+    defer resp.Body.Close()
+
+    payload, ioErr := ioutil.ReadAll(resp.Body)
+
+    if ioErr != nil {
+        log.Fatal(fmt.Sprintf("Unable to read API response: %v", ioErr))
+    }
+
+    ch <- fmt.Sprintf("%s", payload)
+}
+
+func main() {
+    fmt.Println("Initiating SigSci Sound!")
+    fmt.Println("Enjoy the soothing sounds of attacks and anomalies...")
+    fmt.Println("Press Ctrl+C to terminate.")
+    runtime.GOMAXPROCS(2)
+
+    var wg sync.WaitGroup
+    var config Config
+
+    // initialize configuration
+    config = initConfig()
+
+    // add WaitGroups for the number of tags in the configuration
+    // concurrency implementaiton based on https://www.goinggo.net/2014/01/concurrency-goroutines-and-gomaxprocs.html
+    wg.Add(len(config.Tags))
+
+    // set Timeseries endpoint
+    var timeseriesEndpoint = apiURL + "/corps/" + config.CorpName + "/sites/" + config.SiteName + "/timeseries/requests"
  
-    // wiat for WaitGroups
+    api_response_channel := make(chan string)
+    
+    var now = int32(time.Now().Unix())
+
+    for {
+        var fromUntil = fmt.Sprintf("&from=%d&until=%d", now - interval, now)
+        //mixer := new(beep.Mixer)
+
+        // for each tag in configuration launch a goroutine
+        for i := range(config.Tags) {
+            // get tag configuration
+            tag   := config.Tags[i].Name
+            sound := config.Tags[i].Sound
+            endpoint := timeseriesEndpoint + "?tag=" + tag + fromUntil
+
+            go MakeRequest(config.Username, config.Password, endpoint, api_response_channel)
+            
+            var payload = <-api_response_channel
+
+            // initialize Timeseries object and load json payload data
+            var t Timeseries
+
+            unmarshalErr := json.Unmarshal([]byte(payload), &t)
+            if unmarshalErr != nil {
+                log.Fatal(unmarshalErr)
+            }
+
+            // verify sound file exists
+            _, fileErr := os.Stat(sound)
+
+            if fileErr != nil {
+                log.Fatal("Sound file is missing: ", sound)
+            }
+
+            if(0 != len(t.Data)){
+                for i := range(t.Data[0].Data) {
+                    time.Sleep(time.Second)
+                    if(t.Data[0].Data[i] > 0) {
+
+                        f, _ := os.Open(sound)
+                        s, format, _ := wav.Decode(f)
+                        playing := make(chan struct{})
+
+                        //mixer.Play(s)
+                        speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+                        speaker.Play(beep.Seq(s, beep.Callback(func() {
+                            // Callback after the stream Ends
+                            close(playing)
+                        })))
+                        <-playing
+                    }
+                }
+            }
+        }
+
+        // sleep for interval before doing it all over again
+        time.Sleep(time.Second * interval)
+    }
+    // wait for WaitGroups
     wg.Wait()
     fmt.Println("\nTerminating SigSci Sound!")
 }
